ec6a319c2a18bf36ae00457744a789fe1d8b4a1a
Albina <akhmetova235@gmail.ru>
2021-12-02 22:56:29 +0300

    2

--- zadanie_6/1.cpp
+++ zadanie_6/1.cpp
@@ -2,26 +2,22 @@
 # define SIZE_I 3
 # define SIZE_J 3
 
+void    ft_print_array(int **arr, int si, int sj);
+
 void    ft_cinarray(int **arr)
 {
     int i = -1;
     int j = -1;
-    //std::cout << "test\n";
+
     while (++i < SIZE_I)
     {
-        std::cout << "test\n";
         while (++j < SIZE_J)
-        {
-            std::cin >>  arr[i][j];
-            std::cout << i << j << "\n" << arr[i][j] << "\n";
-        }
-        //std::cout << arr[i - 1];
+            std::cin >> arr[i][j];
         j = -1;
     }
-    //std::cout << "test\n";
 }
 
-void    ft_del_array_and_create_new(int ***arr, int mini, int minj)
+int **ft_del_array_and_create_new(int **arr, int mini, int minj)
 {
     int i = -1;
     int **newarr;
@@ -32,7 +28,7 @@ void    ft_del_array_and_create_new(int ***arr, int mini, int minj)
     newarr = new int *[SIZE_I - 1];
     while (++i < SIZE_I - 1)
     {
-        newarr[i] = new int [SIZE_J - j];
+        newarr[i] = new int [SIZE_J - 1];
     }
     i = 0;
     while (++k < SIZE_I)
@@ -41,7 +37,7 @@ void    ft_del_array_and_create_new(int ***arr, int mini, int minj)
         {
             if (k != mini && s != minj)
             {
-                newarr[i][j] = *arr[k][s];
+                newarr[i][j] = arr[k][s];
                 j++;
             }
         }
@@ -50,14 +46,10 @@ void    ft_del_array_and_create_new(int ***arr, int mini, int minj)
             i++;
         j = 0;
     }
-    i = -1;
-    while (++i < SIZE_I)
-        delete (arr[i]);
-    delete (arr);
-    *arr = newarr;
+    return (newarr);
 }
 
-void    ft_find_and_del_min(int ***arr)
+int **ft_find_min(int **arr)
 {
     int i = -1;
     int j = -1;
@@ -69,49 +61,53 @@ void    ft_find_and_del_min(int ***arr)
     {
         while (++j < SIZE_J)
         {
-            if (*arr[i][j] < min)
+            if (arr[i][j] < min)
             {
-                min = *arr[i][j];
+                min = arr[i][j];
                 mini = i;
                 minj = j;
             }
         }
         j = -1;
     }
-    ft_del_array_and_create_new(arr, mini, minj);
+    return (ft_del_array_and_create_new(arr, mini, minj));
 }
 
-void    ft_print_array(int **arr)
+void    ft_print_array(int **arr, int si, int sj)
 {
-    for(int i = 0; i < SIZE_I; ++i)
+    for(int i = 0; i < si; ++i)
     {
-        for(int j = 0; j < SIZE_J; ++j)
+        for(int j = 0; j < sj; ++j)
         {
             std::cout.width(3);
             std::cout << arr[i][j];
         }
         std::cout << "\n";
     }
+    std::cout << "\n";
 }
 
 int main()
 {
     int **arr = new int *[SIZE_I];
     int i = -1;
+    int **tmp;
 
-    //arr = new int *[SIZE_I];
     while (++i < SIZE_I)
-    {
         arr[i] = new int [SIZE_J];
-    }
-    //int arr[SIZE_I][SIZE_J];
-    std::cout << arr[0] << arr[1] << arr[2] << "\n";
-
     std::cout << "Filling up an array : \n";
     ft_cinarray(arr);
-    std::cout << "test\n";
-    ft_print_array(arr);
-    std::cout << "test\n";
-    ft_find_and_del_min(&arr);
-    ft_print_array(arr);
+    ft_print_array(arr, SIZE_I, SIZE_J);
+    tmp = ft_find_min(arr);
+    ft_print_array(tmp, SIZE_I - 1, SIZE_J - 1);
+    i = -1;
+    while (++i < SIZE_I)
+    {
+        delete (arr[i]);
+        if (i < SIZE_I - 1)
+            delete (tmp[i]);
+    }
+    delete (arr);
+    delete (tmp);
+    return (0);
 }
---
+++ zadanie_6/2.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+# define I_1 2
+# define J_1 2
+# define I_2 2
+# define J_2 2
+
+void    ft_print_array(int **arr, int si, int sj)
+{
+    for(int i = 0; i < si; ++i)
+    {
+        for(int j = 0; j < sj; ++j)
+        {
+            std::cout.width(3);
+            std::cout << arr[i][j];
+        }
+        std::cout << "\n";
+    }
+    std::cout << "\n";
+}
+
+void    ft_cinarray(int **arr, int si, int sj)
+{
+    int i = -1;
+    int j = -1;
+
+    while (++i < si)
+    {
+        while (++j < sj)
+            std::cin >> arr[i][j];
+        j = -1;
+    }
+}
+
+int **ft_multiplication(int **arr1, int **arr2)
+{
+    int **mult = new int *[I_1];
+    int i = -1;
+    int j = -1;
+
+    while (++i < I_1)
+        mult[i] = new int [J_2];
+    for (i = 0; i < I_1; i++)
+    {
+        for(j = 0; j < J_2; j++)
+        {
+            mult[i][j] = 0;
+            for (int k = 0; k < J_1; k++)
+                mult[i][j] += arr1[i][k] * arr2[k][j];
+        }
+    }
+    return (mult);
+}
+
+void    ft_delete(int **arr, int si)
+{
+    int i = -1;
+
+    while (++i < si)
+        delete (arr[i]);
+    delete (arr);
+}
+
+int main()
+{
+    int i = -1;
+    int j = -1;
+    int **arr1;
+    int **arr2;
+    int **tmp;
+
+    if (J_1 != I_2)
+    {
+        std::cout << "error\n";
+        return (1);
+    }
+    arr1 = new int *[I_1];
+    arr2 = new int *[I_2];
+    while (++i < I_1)
+        arr1[i] = new int [J_1];
+    while (++j < I_2)
+        arr2[j] = new int [J_2];
+    ft_cinarray(arr1, I_1, J_1);
+    ft_cinarray(arr2, I_2, J_2);
+    ft_print_array(arr1, I_1, J_1);
+    ft_print_array(arr2, I_2, J_2);
+    tmp = ft_multiplication(arr1, arr2);
+    ft_print_array(tmp, I_1, J_2);
+    ft_delete(arr1, I_1);
+    ft_delete(arr2, I_2);
+    ft_delete(tmp, I_1);
+    return (0);
+}
